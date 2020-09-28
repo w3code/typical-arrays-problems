91525a85f4edf55158b704c606373d7a14cec9ae
Alex <qatechnix@gmail.com>
2020-09-29 02:13:24 +0700

    refactor: typical arrays problems task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+      let indexOfPosition = 0;
+        for (let j = 1; j < array.length; j++) {
+            if (array[indexOfPosition] > array[j]) {
+                indexOfPosition = j;
+            }
+        }
+        return array[indexOfPosition];
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+      let indexOfPosition = 0;
+        for (let j = 1; j < array.length; j++) {
+            if (array[indexOfPosition] < array[j]) {
+                indexOfPosition = j;
+            }
+        }
+        return array[indexOfPosition]; 
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+      let avgSumm = 0;
+        for (let j = 0; j < array.length; j++) {
+            avgSumm = avgSumm + array[j];
+        }
+        return avgSumm / array.length;
+    }
 }
